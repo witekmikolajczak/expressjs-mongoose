8e54b1ded57faf9138df05045cedfc71b35a96f7
witekmikolajczak <witek.mikolajczak@e.askwho.net>
2022-12-05 16:43:55 +0100

    update prod env

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 web: yarn start
-dashboard: yarn dashboard
--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,53 @@
 import express from 'express';
 require('dotenv').config();
 const ParseServer = require('parse-server').ParseServer;
-const path = require('path');
-console.log(__dirname);
+const ParseDashboard = require('parse-dashboard');
 
-const MASTER_KEY = process.env.MASTER_KEY || 'masterKey';
-const CLOUD_CODE_MAIN = 'src/cloud/main.ts';
-const APP_ID = process.env.APP_ID || 'myAppId';
-const parse_port = process.env.PARSE_PORT || 1337; //  process.env.SERVER_URL ||
-const SERVER_URL = `http://localhost:${parse_port}/parse`; // Don't forget to change to https if needed
-let DATABASE_URI =
-  process.env.DATABASE_URI || process.env.MONGODB_URI;
+const PARSE_DASHBOARD_USERNAME = process.env.PARSE_DASHBOARD_USERNAME 
+const PARSE_DASHBOARD_PASSWORD = process.env.PARSE_DASHBOARD_PASSWORD
+const PARSE_SERVER_MASTER_KEY = process.env.PARSE_SERVER_MASTER_KEY;
+const PARSE_SERVER_APP_ID = process.env.PARSE_SERVER_APP_ID;
+const PARSE_SERVER_APP_NAME = process.env.PARSE_SERVER_APP_NAME;
+const PARSE_SERVER_PORT = process.env.PARSE_SERVER_PORT;
+const PARSE_SERVER_URL = process.env.PARSE_SERVER_URL;
+const PARSE_SERVER_DATABASE_URI = process.env.MONGO_DATABASE_URI;
+const PARSE_SERVER_CLOUD = 'src/cloud/main.ts';
 
-if (!DATABASE_URI) {
-  DATABASE_URI = 'mongodb://localhost:27017/dev';
-  console.log(
-    `DATABASE_URI not specified, falling back to ${DATABASE_URI}`
-  );
-}
+if (!PARSE_SERVER_DATABASE_URI) throw 'DB URI NOT DEFINED'
 
 const api = new ParseServer({
-  databaseURI: DATABASE_URI,
-  cloud: CLOUD_CODE_MAIN,
-  appId: APP_ID,
-  masterKey: MASTER_KEY,
-  serverURL: SERVER_URL,
+  databaseURI: PARSE_SERVER_DATABASE_URI,
+  cloud: PARSE_SERVER_CLOUD,
+  appId: PARSE_SERVER_APP_ID,
+  masterKey: PARSE_SERVER_MASTER_KEY,
+  serverURL: PARSE_SERVER_URL,
 });
-const app = express();
+const dashboard = new ParseDashboard({
+  apps: [
+    {
+      serverURL: PARSE_SERVER_URL,
+      appId: PARSE_SERVER_APP_ID,
+      masterKey: PARSE_SERVER_MASTER_KEY,
+      appName: PARSE_SERVER_APP_NAME,
+    },
+  ],
+  users: [
+    {
+      user: PARSE_DASHBOARD_USERNAME,
+      pass: PARSE_DASHBOARD_PASSWORD
+    },
+  ],
+});
 
-const port = process.env.PORT || 3333;
+const app = express();
 
 app.use(express.json());
 app.use(express.raw({ type: 'application/vnd.custom-type' }));
 app.use(express.text({ type: 'text/html' }));
 
-app.get('/', async (req, res) => {
-  res.json({
-    message: 'Please visit /countries to view all the countries',
-  });
-});
-
 app.use('/parse', api);
+app.use('/dashboard', dashboard);
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
-
-app.listen(parse_port, function () {
-  console.log(
-    'parse-server-example running on port  1337.',
-    SERVER_URL
-  );
+app.listen(PARSE_SERVER_PORT, () => {
+  console.log(`Example app listening at http://localhost:${PARSE_SERVER_PORT}`);
 });
